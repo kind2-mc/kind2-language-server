9695e436ef6fdcc3c2cd84dd7dc0cb6b12d0ba34
Josh <jbergthold@uiowa.edu>
2026-06-17 23:06:03 -0500

    Add support for lemmas

--- src/main/java/edu/uiowa/kind2/lsp/Kind2LanguageServer.java
+++ src/main/java/edu/uiowa/kind2/lsp/Kind2LanguageServer.java
@@ -76,6 +76,7 @@ import edu.uiowa.cs.clc.kind2.results.AstInfo;
 import edu.uiowa.cs.clc.kind2.results.ConstDeclInfo;
 import edu.uiowa.cs.clc.kind2.results.ContractInfo;
 import edu.uiowa.cs.clc.kind2.results.FunctionInfo;
+import edu.uiowa.cs.clc.kind2.results.LemmaInfo;
 import edu.uiowa.cs.clc.kind2.results.Log;
 import edu.uiowa.cs.clc.kind2.results.NodeInfo;
 import edu.uiowa.cs.clc.kind2.results.NodeResult;
@@ -266,7 +267,7 @@ public class Kind2LanguageServer
       if (parseResults.containsKey(uri)) {
         try {
           for (AstInfo info : parseResults.get(uri).getAstInfos()) {
-            if (info instanceof NodeInfo || info instanceof FunctionInfo || info instanceof TypeDeclInfo || info instanceof ConstDeclInfo) {
+            if (info instanceof NodeInfo || info instanceof FunctionInfo || info instanceof TypeDeclInfo || info instanceof ConstDeclInfo || info instanceof LemmaInfo) {
               client.logMessage(new MessageParams(MessageType.Info, info.getJson()));
               components.add(replacePathWithUri(info.getJson(), uri,
                   info.getFile() == null ? new URI(uri).getPath()
@@ -935,6 +936,7 @@ private MCSCategory stringToMCSCategory(String cat){
         api.setLusMainConst(name);
         break;
       case "nodeDecl":
+      case "lemmaDecl":
         api.setLusMain(name);
         break;
       case "typeDecl":
@@ -1073,7 +1075,9 @@ private MCSCategory stringToMCSCategory(String cat){
                   kind = SymbolKind.Function;
                 } else if (info instanceof ContractInfo) {
                   kind = SymbolKind.Interface;
-                } else {
+                } else if (info instanceof LemmaInfo) {
+                  kind = SymbolKind.Function;
+                }else {
                   kind = null;
                 }
                 symbols.add(Either.forRight(
